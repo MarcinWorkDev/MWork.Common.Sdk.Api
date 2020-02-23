cbf583d0da158b754dcdb908dd3f3878183e06f7
Marcin Li <marcin93li@hotmail.com>
2020-02-23 22:54:18 +0100

    init

---
+++ MWork.Common.Sdk.Api/Dtos/CursorDto.cs
@@ -0,0 +1,9 @@
+namespace MWork.Common.Sdk.Api.Dtos
+{
+    public class CursorDto
+    {
+        public string Prev { get; set; }
+        
+        public string Next { get; set; }
+    }
+}
---
+++ MWork.Common.Sdk.Api/Dtos/ErrorDto.cs
@@ -0,0 +1,9 @@
+namespace MWork.Common.Sdk.Api.Dtos
+{
+    public class ErrorDto
+    {
+        public int StatusCode { get; set; }
+        
+        public string Message { get; set; }
+    }
+}
